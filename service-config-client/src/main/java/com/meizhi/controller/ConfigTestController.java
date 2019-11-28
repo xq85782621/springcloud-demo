cf6f6e9d64a4b160b139d187a4908ccdaac4b53f
Administrator <85782621@qq.com>
2019-11-28 16:02:43 +0800

    2019-11-27

--- service-config-client/src/main/java/com/meizhi/controller/ConfigTestController.java
+++ service-config-client/src/main/java/com/meizhi/controller/ConfigTestController.java
@@ -3,6 +3,7 @@ package com.meizhi.controller;
 
 import com.meizhi.common.response.ResponseResult;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/")
+@RefreshScope //加这个注解才能获得刷新后的配置(即使动态修改了端口,在这里的port被改变了,但是项目的端口依然不会改变)
 public class ConfigTestController {
 
     /**
